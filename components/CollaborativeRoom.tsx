0c2aea327c84450c49f97a490585157c99a5f9b4
arafat-alim <arafat97alim@gmail.com>
2024-11-06 20:00:53 +0530

    ➕ Collaborative Room component added.

---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,36 @@
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
+import Header from "./Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Editor } from "./editor/Editor";
+import Loader from "./Loader";
+
+const CollaborativeRoom = () => {
+  return (
+    <RoomProvider id="my-room">
+      <ClientSideSuspense fallback={<Loader />}>
+        <div className="collaborative-room">
+          <Header className="header">
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title">Share</p>
+            </div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton
+                appearance={{
+                  elements: {
+                    avatarBox: "w-[60px] h-[60px]",
+                  },
+                }}
+              />
+            </SignedIn>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
