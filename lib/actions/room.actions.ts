fce8534c56be3b3233b1aba20534c001bc38ba13
arafat-alim <arafat97alim@gmail.com>
2024-11-07 17:45:56 +0530

    ➕ Action added: Fetch all document from liveblocks

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -86,3 +86,12 @@ export const updateDocument = async (roomId: string, title: string) => {
     );
   }
 };
+
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+    return parseStringify(rooms);
+  } catch (err) {
+    console.log("Error occured while fetching the rooms");
+  }
+};
