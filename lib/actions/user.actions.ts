a6a30d58eaa411a78368187acd0b9089d5c6fed5
arafat-alim <arafat97alim@gmail.com>
2024-11-07 21:18:20 +0530

    ✨ Feat: Added a realtime mention other users or tag user.

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { clerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
   try {
@@ -23,5 +24,37 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
     return parseStringify(sortedUsers);
   } catch (error) {
     console.log(`Error fetching users: ${error}`);
+  }
+};
+
+export const getDocumentUsers = async ({
+  roomId,
+  text,
+  currentUser,
+}: {
+  roomId: string;
+  text: string;
+  currentUser: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+    //! All users excluding me
+    const users = Object.keys(room.usersAccesses).filter(
+      (email) => email !== currentUser
+    );
+
+    //! Search User
+    if (text.length) {
+      const lowerCaseText = text.toLowerCase();
+
+      const filteredUsers = users.filter((email: string) =>
+        email.toLowerCase().includes(lowerCaseText)
+      );
+      return parseStringify(filteredUsers);
+    }
+
+    return parseStringify(users);
+  } catch (err) {
+    console.log(`Error occured while fetching documentUsers: ${err}`);
   }
 };
