f569c2d589da8f6ed7d3b2ce1451efa7a1943eed
arafat-alim <arafat97alim@gmail.com>
2024-11-07 16:46:55 +0530

    ✨ Implemented the feature: Document title can now be changed and stop the input box on clicking outside, prese Enter and ESC key.

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -6,18 +6,94 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Editor } from "./editor/Editor";
 import Loader from "./Loader";
 import ActiveCollaborators from "./ActiveCollaborators";
+import { useEffect, useRef, useState } from "react";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
 
 const CollaborativeRoom = ({
   roomId,
   roomMetadata,
 }: CollaborativeRoomProps) => {
+  const currentUserType = "editor";
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
+  const [editing, setEditing] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLDivElement>(null);
+  const updateTitleHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {};
+
+  //! Click out the input close the title editor
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(e.target as Node)
+      ) {
+        setEditing(false);
+      }
+    };
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        // Revert changes logic here
+        setEditing(false);
+      } else if (e.key === "Enter") {
+        // Save changes logic here
+        setEditing(false);
+      }
+    };
+
+    //! Adding event listeners
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleKeyDown);
+
+    //! Clean up event listeners
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
     <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header className="header">
-            <div className="flex w-fit items-center justify-center gap-2">
-              <p className="document-title">Share</p>
+            <div
+              ref={containerRef}
+              className="flex w-fit items-center justify-center gap-2"
+            >
+              {editing && !loading ? (
+                <Input
+                  type="text"
+                  value={documentTitle}
+                  ref={inputRef}
+                  onChange={(e) => setDocumentTitle(e.target.value)}
+                  onKeyDown={updateTitleHandler}
+                  disabled={!editing}
+                  className="document-title-input"
+                />
+              ) : (
+                <>
+                  <p className="document-title">{documentTitle}</p>
+                </>
+              )}
+              {currentUserType === "editor" && !editing && (
+                <Image
+                  src="/assets/icons/edit.svg"
+                  alt="edit"
+                  width={24}
+                  height={24}
+                  onClick={() => setEditing(true)}
+                  className="pointer"
+                />
+              )}
+              {currentUserType !== "editor" && !editing && (
+                <p className="view-only-tag">View Only</p>
+              )}
+
+              {loading && <p className="text-sm text-gray-400">Saving...</p>}
             </div>
             <div className="flex w-full flex-1 gap-2 justify-end sm:gap-3">
               <ActiveCollaborators />
