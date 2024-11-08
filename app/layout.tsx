4f56197c17496e274c81df48cd456c406aba4955
arafat-alim <arafat97alim@gmail.com>
2024-11-08 21:28:46 +0530

    ✨ SEO Meta tags implemented.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,31 @@ export const metadata: Metadata = {
   title: "Sync Pad - Bringing Teams Together, One Idea at a Time",
   description:
     "A seamless online collaboration platform that brings your team together to create, edit, and share documents in real-time. Experience effortless teamwork, anytime, anywhere.",
+  openGraph: {
+    title: "Sync Pad - Bringing Teams Together, One Idea at a Time",
+    description:
+      "A seamless online collaboration platform that brings your team together to create, edit, and share documents in real-time. Experience effortless teamwork, anytime, anywhere.",
+    url: "https://yourwebsite.com",
+    siteName: "Sync Pad",
+    images: [
+      {
+        url: "https://res.cloudinary.com/cocoder/image/upload/w_430/v1731081371/Projects/SyncPad/logo_babb36.png", // Replace with your actual logo URL
+        width: 1200,
+        height: 630,
+        alt: "Sync Pad Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sync Pad - Bringing Teams Together, One Idea at a Time",
+    description:
+      "A seamless online collaboration platform that brings your team together to create, edit, and share documents in real-time.",
+    images: [
+      "https://res.cloudinary.com/cocoder/image/upload/w_430/v1731081371/Projects/SyncPad/logo_babb36.png",
+    ], // Replace with your actual logo URL
+  },
 };
 
 export default function RootLayout({
